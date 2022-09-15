3671afbe99ecc5b237ecdd9077795f0442ddaa96
Eliza Milburn <61363431+ERmilburn02@users.noreply.github.com>
2022-09-16 00:10:44 +0100

    Styled the about page
    Co-authored-by: TJCPlays <104585300+TJCPlays@users.noreply.github.com>

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,10 +5,10 @@ import md from "markdown-it";
 import CustomImage from "../components/CustomImage";
 
 export async function getStaticProps() {
-  const files = fs.readdirSync("TeamBios");
+  const files = fs.readdirSync("teamBios");
   const people = files.map((fileName) => {
     const slug = fileName.replace(".md", "");
-    const readFile = fs.readFileSync(`teambios/${fileName}`, "utf-8");
+    const readFile = fs.readFileSync(`teamBios/${fileName}`, "utf-8");
     const { data: frontmatter, content } = matter(readFile);
     const htmlContent = md({ html: true, xhtmlOut: true }).render(content);
 
@@ -31,19 +31,23 @@ export default function About({
   }[];
 }) {
   return (
-    <>
-      <h1>About us</h1>
-      <p>
-        We are a small indie game studio, working on games for the entire
-        family.
-      </p>
-      <div>
-        <h3>Meet the team</h3>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-4 md:p-0">
+    <div className="text-gray-500 dark:text-gray-400 text-center">
+      <div className="py-2">
+        <h1 className="text-[#dcdfe7] underline text-3xl lg:text-4xl font-bold pb-5">
+          About us
+        </h1>
+        <p>
+          We are a small indie game studio, working on games for the entire
+          family.
+        </p>
+      </div>
+      <div className="py-2">
+        <h3 className="text-2xl underline lg:text-3xl">Meet the team</h3>
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-4">
           {people.map(({ slug, frontmatter, htmlContent }) => (
             <div
               key={slug}
-              className="border border-gray-200 dark:border-gray-800 m-2 rounded-2xl shadow-lg overflow-hidden flex flex-col text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+              className="border border-gray-200 dark:border-gray-800 m-2 rounded-2xl shadow-lg overflow-hidden flex flex-col text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white text-start"
             >
               <CustomImage
                 width={240}
@@ -53,11 +57,11 @@ export default function About({
                 src={`/${frontmatter.pfp}`}
               />
               <h1 className="p-4 pb-2 font-bold">{frontmatter.name}</h1>
-              <p className="p-4 pt-0">{frontmatter.bio}</p>
+              <p className="p-4 pt-0">{frontmatter.role}</p>
             </div>
           ))}
         </div>
       </div>
-    </>
+    </div>
   );
 }
